ff33d6c092444fbaf3a6921b71d07968bcda56a9
janderton987 <jasperanderton@gmail.com>
2025-01-27 17:39:02 -0800

    Uninished Lab 2

---
+++ PyLab02.py
@@ -0,0 +1,35 @@
+name = "Jasper"
+age = 44
+height = 6
+favorite_color = "Red"
+
+print(f"""{name}
+Age: {age}
+Height: {height}
+Favorite Color {favorite_color}
+""")
+
+import math
+
+r = int(input("Enter a circle radius: "))
+circle_area = math.pi * math.pow(r,2)
+print(f"Circle Area with radius {r} is {circle_area:.1f}")
+
+print(fsqrt"Square Root of my age is {math.sqrt(age)}")
+
+print{f:sin of height: {math.sin(height)}, cos of height: {math.cos{height)}")
+
+print:(f"Sum of Age and 5: {age + 5}")
+
+print(f"the difference between my height and 4 is {height - 43}")
+
+print(f"The product of my age and height is: {age * height}")
+
+print(f"the quotient of height and 2 is: {height / 2}")
+
+print(f"The remainder of my age divided by 3 {age % (age + 1)}")
+
+print(f"Age raised to the power of 2 {age_raised_to_2}")
+
+temp_Fahrenheit = input("enter a temperature in Fahrenheit: ")
+celsius = (temp_Fahrenheit - 32) * (5/9)
