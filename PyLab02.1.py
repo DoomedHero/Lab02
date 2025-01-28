ea0feddbda343ca05339803572b13135bf8156e8
janderton987 <jasperanderton@gmail.com>
2025-01-27 17:44:23 -0800

    Finished Lab 2

--- PyLab02.1.py
+++ PyLab02.1.py
@@ -15,11 +15,11 @@ r = int(input("Enter a circle radius: "))
 circle_area = math.pi * math.pow(r,2)
 print(f"Circle Area with radius {r} is {circle_area:.1f}")
 
-print(fsqrt"Square Root of my age is {math.sqrt(age)}")
+print(f"Square Root of my age is {math.sqrt(age)}")
 
-print{f:sin of height: {math.sin(height)}, cos of height: {math.cos{height)}")
+print(f"sin of height: {math.sin(height)}, cos of height: {math.cos(height)}")
 
-print:(f"Sum of Age and 5: {age + 5}")
+print(f"Sum of Age and 5: {age + 5}")
 
 print(f"the difference between my height and 4 is {height - 43}")
 
@@ -29,8 +29,8 @@ print(f"the quotient of height and 2 is: {height / 2}")
 
 print(f"The remainder of my age divided by 3 {age % (age + 1)}")
 
-print(f"Age raised to the power of 2 {age_raised_to_2}")
+print(f"Age raised to the power of 2 {age ** 2}")
 
-temp_Fahrenheit = input("enter a temperature in Fahrenheit: ")
-celsius = (temp_Fahrenheit - 32) * (5/9)
-print("Temp Fahrenheit {temp_fahrenheit}°F in Celsius is {celsius}°C")
+temp_fahrenheit = int(input("enter a temperature in Fahrenheit: "))
+celsius = (temp_fahrenheit - 32) * (5/9)
+print(f"Temp Fahrenheit {temp_fahrenheit}°F in Celsius is {celsius}°C")
